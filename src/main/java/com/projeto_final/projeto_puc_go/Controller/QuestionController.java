dc4c0270c05b32d4076fc1846534e566ea366588
Filipe <filipecj0@gmail.com>
2025-05-13 18:56:38 -0300

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/main/java/com/projeto_final/projeto_puc_go/Controller/QuestionController.java
    #	src/main/java/com/projeto_final/projeto_puc_go/Service/QuestionService.java

--- src/main/java/com/projeto_final/projeto_puc_go/Controller/QuestionController.java
+++ src/main/java/com/projeto_final/projeto_puc_go/Controller/QuestionController.java
@@ -3,9 +3,13 @@ package com.projeto_final.projeto_puc_go.Controller;
 import com.projeto_final.projeto_puc_go.Entity.Question;
 import com.projeto_final.projeto_puc_go.Service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping; // Importação da anotação @GetMapping
+import org.springframework.web.bind.annotation.RequestMapping; // Importação da anotação @RequestMapping
+import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@RequestMapping("/questions") // Mapeia a requisição para /questions
 public class QuestionController {
 
     private final QuestionService questionService;
@@ -15,5 +19,8 @@ public class QuestionController {
         this.questionService = questionService;
     }
 
+    @GetMapping // Mapeia requisições GET para /questions
+    public List<Question> getAllQuestions() {
+        return questionService.getAllQuestions();
     }
 }
